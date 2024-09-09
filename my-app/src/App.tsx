df8554535be854d03151a1c17fbc9954b7744d42
laadiim <112978764+laadiim@users.noreply.github.com>
2024-09-09 10:42:51 +0200

    regForm

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import RegisterForm from './components/registerForm';
 
 function App() {
   return (
-		
+		<RegisterForm/>		
   );
 }
 
--- my-app/src/components/registerForm.tsx
+++ my-app/src/components/registerForm.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { Component, } from "react";
+import Register0 from "./register0";
 
-export default RegisterForm = () => {
-	return (
-		
-	)
+class RegisterForm extends Component {
+	
+	constructor(props: any) {
+		super(props);
+		this.state = {
+			stage : 0,
+			firstName : "",
+			lastName : "",
+			username : "",
+			email : "",
+			password : "",
+			address : {
+				number : "",
+				street : "",
+				city : "",
+				country : "",
+			},
+			phone : "",
+			photo : "",
+		};
+	}
+
+	render() {
+		return (
+			<div>
+				<Register0/>
+			</div>
+		)
+	}
 }
+
+export default RegisterForm;
